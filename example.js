0db988e108af7a7b50b8b237c4c1547b4a071e51
Fenzakosu <lukakovacevic266@gmail.com>
2025-04-29 07:48:21 +0200

    Refaktorisana funkcija za ubacivanje artikala u tabelu.

--- example.js
+++ example.js
@@ -21,36 +21,47 @@ tabela.style.border = "1px solid black";
 
 function prikaziArtikle(artikli) {
     for (let artikal of artikli) {
-        let tr = document.createElement("tr");
 
-        let id = document.createElement("td");
-        let naziv = document.createElement("td");
-        let cena = document.createElement("td");
-        let dostupnost = document.createElement("td");
-
-        id.textContent = artikal.id;
-        naziv.textContent = artikal.naziv;
-        cena.textContent = artikal.cena;
-
-        id.style.border = "1px solid black";
-        naziv.style.border = "1px solid black";
-        cena.style.border = "1px solid black";
-        dostupnost.style.border = "1px solid black";
-
-        if (artikal.dostupnost) {
-            dostupnost.textContent = "DA";
-        } else {
-            dostupnost.textContent = "NE";
-            tr.style.backgroundColor = "#F8CECC";
-        }
-
-        tr.appendChild(id);
-        tr.appendChild(naziv);
-        tr.appendChild(cena);
-        tr.appendChild(dostupnost);
+        let tr = kreirajRed(artikal);
 
         tabela.appendChild(tr);
     }
+}
+
+function stilizujRed(td) {
+    td.style.border = "1px solid black";
+}
+
+function kreirajRed(artikal) {
+    let tr = document.createElement("tr");
+
+    let id = document.createElement("td");
+    let naziv = document.createElement("td");
+    let cena = document.createElement("td");
+    let dostupnost = document.createElement("td");
+
+    id.textContent = artikal.id;
+    naziv.textContent = artikal.naziv;
+    cena.textContent = artikal.cena;
+
+    stilizujRed(id);
+    stilizujRed(naziv);
+    stilizujRed(cena);
+    stilizujRed(dostupnost);
+
+    if (artikal.dostupnost) {
+        dostupnost.textContent = "DA";
+    } else {
+        dostupnost.textContent = "NE";
+        tr.style.backgroundColor = "#F8CECC";
+    }
+
+    tr.appendChild(id);
+    tr.appendChild(naziv);
+    tr.appendChild(cena);
+    tr.appendChild(dostupnost);
+
+    return tr;
 }
 
 prikaziArtikle(artikli);
